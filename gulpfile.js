32e58e10f10f7f23f0edc6633ebd675a8d8910e9
Roman Artemyev <artemyev.ram@gmail.com>
2023-11-09 15:55:39 +0300

    Добавил структуру Swiper Добавлен блок info добавлен блок welcome goods Правки в прижатый футер (Отсутствовал скролл)

--- gulpfile.js
+++ gulpfile.js
@@ -216,7 +216,7 @@ const resources = () => {
 }
 
 const images = () => {
-  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`])
+  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg,webp}`])
     .pipe(gulpif(isProd, image([
       image.mozjpeg({
         quality: 80,
@@ -230,7 +230,7 @@ const images = () => {
 };
 
 const webpImages = () => {
-  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`])
+  return src([`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,webp}`])
     .pipe(webp())
     .pipe(dest(paths.buildImgFolder))
 };
@@ -260,8 +260,8 @@ const watchFiles = () => {
   watch(`${paths.srcPartialsFolder}/*.html`, htmlInclude);
   watch(`${srcFolder}/*.html`, htmlInclude);
   watch(`${paths.resourcesFolder}/**`, resources);
-  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`, images);
-  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png}`, webpImages);
+  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg,webp}`, images);
+  watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,webp}`, webpImages);
   watch(paths.srcSvg, svgSprites);
 }
 
